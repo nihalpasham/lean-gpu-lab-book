dd79cdc4075dead56c178ec5ede36d7065d55742
nihalp <nihalp@nvidia.com>
2026-09-15 20:23:51 +0530

    Publish book from lean-gpu-lab@d644f69

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1fab6641.js',
+                path_to_root + 'searchindex-59277be9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
